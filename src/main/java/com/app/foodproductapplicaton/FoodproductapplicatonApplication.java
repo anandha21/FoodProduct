83d47030cce05e578f6bacea04df82376191dc8a
Anandha Krishnan S <anandhakrish86@gmail.com>
2021-09-11 20:06:04 +0530

    First Commit

---
+++ src/main/java/com/app/foodproductapplicaton/FoodproductapplicatonApplication.java
@@ -0,0 +1,13 @@
+package com.app.foodproductapplicaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodproductapplicatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodproductapplicatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/foodproductapplicaton/FoodproductapplicatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.foodproductapplicaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodproductapplicatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
